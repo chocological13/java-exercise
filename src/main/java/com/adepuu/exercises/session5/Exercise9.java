da8b1227c7b88169a31a3a791fcceb2deaf22110
nina <annisamustofa@outlook.com>
2024-03-26 06:31:07 +0700

    feat: ex9 tempDays brute force prolly

--- src/main/java/com/adepuu/exercises/session5/Exercise9.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise9.java
@@ -1,22 +1,57 @@
 package com.adepuu.exercises.session5;
 
-public class Exercise9 {
-    /**
-     * Java Program to get the number of days you have to wait after the i-th day to get a warmer temperature
-     *
-     * Example 1:
-     * Input: temperatures = [73,74,75,71,69,72,76,73]
-     * Output: [1,1,4,2,1,1,0,0]
-     *
-     * Example 2:
-     * Input: temperatures = [30,40,50,60]
-     * Output: [1,1,1,0]
-     *
-     * Example 3:
-     * Input: temperatures = [30,60,90]
-     * Output: [1,1,0]
-     */
-    public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+public class Exercise9 {
+
+  /**
+   * Java Program to get the number of temps you have to wait after the i-th day to get a warmer temperature
+   * <p>
+   * Example 1: Input: temperatures = [73,74,75,71,69,72,76,73] Output: [1,1,4,2,1,1,0,0]
+   * <p>
+   * Example 2: Input: temperatures = [30,40,50,60] Output: [1,1,1,0]
+   * <p>
+   * Example 3: Input: temperatures = [30,60,90] Output: [1,1,0]
+   */
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    System.out.print("Enter array size: ");
+    int arraySize = scan.nextInt();
+
+    List<Integer> temps = new ArrayList<>();
+    System.out.println("Enter temperatures: ");
+    for (int i = 0; i < arraySize; i++) {
+      int num = scan.nextInt();
+      temps.add(num);
     }
+
+    System.out.println("Your current array of temperature is: " + temps);
+
+    List<Integer> tempDays = new ArrayList<>();
+    for (int i = 0; i < arraySize; i++) {
+//      System.out.println("iteration i: " + i + " " + tempDays);
+      for (int j = i + 1; j < arraySize; j++) {
+        if (temps.get(j) > temps.get(i)) {
+          int dayDiff = j - i;
+          tempDays.add(dayDiff);
+          break;
+        }
+//        System.out.println("iteration i: " + i + " and j: " + j + " " + tempDays);
+      }
+
+      // if there are no warmer days after temp at day(i), we use try catch method to see if the array list in that
+      // index is null (it doesn't exist bc we didn't add anything to it from the if statement) and assign 0 to it.
+      try {
+        tempDays.get(i);
+      } catch (IndexOutOfBoundsException e) {
+        tempDays.add(i, 0);
+      }
+//      System.out.println("current array: " + tempDays);
+    }
+
+    System.out.print("How many days till it's warmer: " + tempDays);
+  }
 }
