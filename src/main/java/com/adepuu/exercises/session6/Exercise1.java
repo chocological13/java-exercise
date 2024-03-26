af693a9928e76407fbc51115222f54d14b453d18
nina <annisamustofa@outlook.com>
2024-03-26 13:47:27 +0700

    feat: ex1 and ex2

--- src/main/java/com/adepuu/exercises/session6/Exercise1.java
+++ src/main/java/com/adepuu/exercises/session6/Exercise1.java
@@ -1,18 +1,62 @@
 package com.adepuu.exercises.session6;
 
-public class Exercise1 {
-    /**
-     * Write a Java Program to Enter numbers to calculate average and enter 'q' to finish.
-     *
-     * Input: 1, 2, 3, 4, 5, q
-     * Output: 3
-     *
-     * Input: 1, 2, 3, a, b, 4, 5, q
-     * Output: 3
-     *
-     * Explanation: print “Invalid input. Please enter a valid number or 'q' to finish.” if user not inserting the expected number or character.
-     */
-    public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
 
+public class Exercise1 {
+
+  /**
+   * Write a Java Program to Enter numbers to calculate average and enter 'q' to finish.
+   * <p>
+   * Input: 1, 2, 3, 4, 5, q Output: 3
+   * <p>
+   * Input: 1, 2, 3, a, b, 4, 5, q Output: 3
+   * <p>
+   * Explanation: print &ldquo;Invalid input. Please enter a valid number or 'q' to finish.&rdquo; if user not inserting the
+   * expected number or character.
+   */
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    List<Integer> numbers = new ArrayList<>();
+    // initialize boolean
+    boolean validInput = true;
+
+    System.out.println("Please enter numbers to average. Press 'q' to stop the input.");
+    while (validInput) {
+    try {
+      if (scan.hasNextInt()) {
+        int num = scan.nextInt();
+        numbers.add(num);
+      } else {
+        String exit = scan.next();
+        if (exit.equalsIgnoreCase("q")) {
+          validInput = false;
+        } else {
+          throw new InputMismatchException("Please enter number or 'q' to stop input");
+        }
+      }
+      } catch (InputMismatchException e) {
+        System.out.println(e.getMessage());
+        validInput = true;
+      }
+      scan.nextLine();
     }
+    scan.close();
+
+    System.out.println("Your numbers are: " + numbers);
+
+    System.out.println("Your average is: " + average(numbers));
+  }
+
+  public static float average(List<Integer> numbers) {
+    float total = 0;
+    for (int number : numbers) {
+      total += number;
+    }
+
+    return total / numbers.size();
+  }
 }
--- src/main/java/com/adepuu/exercises/session6/Exercise2.java
+++ src/main/java/com/adepuu/exercises/session6/Exercise2.java
@@ -2,6 +2,8 @@ package com.adepuu.exercises.session6;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Exercise2 {
     /**
@@ -12,16 +14,33 @@ public class Exercise2 {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the file name: ");
         String fileName = scanner.nextLine();
-        readFileContents(fileName);
+        try {
+          readFileContents(fileName);
+        } catch (NullPointerException e) {
+          e.getCause();
+        }
         scanner.close();
     }
 
     static void readFileContents(String fileName) {
         BufferedReader reader = null;
-        reader = new BufferedReader(new FileReader(fileName));
-        String line;
-        while ((line = reader.readLine()) != null) {
+        try {
+          reader = new BufferedReader(new FileReader(fileName));
+          String line;
+          while ((line = reader.readLine()) != null) {
             System.out.println(line);
+          }
+        } catch (IOException e) {
+          System.out.println("There was an error opening file " + e.getMessage());
+        } finally{
+          if (reader != null) {
+            try {
+              reader.close();
+            } catch (IOException e2) {
+              System.out.println("Now there's an error closing the file D:");
+              e2.printStackTrace();
+            }
+          }
         }
     }
 }
