299aa66e5db77215f061583ba996bfff09654cbf
nina <annisamustofa@outlook.com>
2024-04-02 16:16:51 +0700

    idk y my head is null after dequeue :(

--- src/main/java/com/adepuu/exercises/session11/Queue.java
+++ src/main/java/com/adepuu/exercises/session11/Queue.java
@@ -1,5 +1,8 @@
 package com.adepuu.exercises.session11;
 
+import java.util.ArrayList;
+import java.util.EmptyStackException;
+
 public class Queue {
     /**
      * Write a Java queue program that can scale dynamically
@@ -16,4 +19,113 @@ public class Queue {
      * - Memory Efficiency: The queue should use memory efficiently, allocating and deallocating memory dynamically as needed.
      * - Error Handling: The program should handle edge cases gracefully, such as attempting to dequeue an element from an empty queue, and provide clear error messages.
      */
+    private Node head;
+    private Node tail;
+    int queueSize;
+
+    private static class Node {
+      Node next;
+      int data;
+
+      Node(int data) {
+        this.data = data;
+        this.next = null;
+      }
+    }
+
+    public Queue() {
+      head = null;
+      tail = null;
+      queueSize = 0;
+    }
+
+    public boolean isEmpty() {
+      return this.queueSize == 0;
+    }
+
+    public int getQueueSize() {
+      return queueSize;
+    }
+
+    private void enqueue(int data) {
+      Node newNode = new Node(data);
+
+      if (head == tail) {
+        head = newNode;
+        tail = newNode;
+      } else {
+        tail.next = newNode;
+        tail = newNode;
+      }
+      queueSize++;
+    }
+
+    public void dequeue() {
+      try {
+        if (isEmpty()) {
+          throw new EmptyStackException();
+        } else {
+//          Node dequeuedNode = head;
+          head = head.next;
+
+          if(head == null) {
+            tail = null;
+          }
+
+//          dequeuedNode.next = null;
+          queueSize--;
+          System.out.println("Data dequeued!");
+        }
+      } catch (EmptyStackException e) {
+        System.out.println("Queue is currently empty, nothing to dequeue!");
+      }
+    }
+
+    private void peek() {
+      try {
+        if (!isEmpty()) {
+          System.out.println("Current queue size is: " + this.getQueueSize());
+          System.out.println("Data at the top of the queue is: " + this.head);
+        } else {
+          throw new EmptyStackException();
+        }
+      } catch (EmptyStackException e) {
+        System.out.println("Queue is empty, no data to show!");
+      }
+    }
+
+    // ? I can't really figure this out, apparently current.next is null
+    // ? Not sure if I path it wrongly
+//    public void display() {
+//      try {
+//        if (!isEmpty()) {
+//          ArrayList<Integer> queueData = new ArrayList<>();
+//
+//          Node current = this.head;
+//          while (current != null) {
+//            queueData.add(current.data);
+//            current = current.next;
+//          }
+//
+//          System.out.println("Your current queue data is: " + queueData.toString());
+//        } else {
+//          throw new IllegalStateException();
+//        }
+//      } catch (IllegalStateException e) {
+//        System.out.println("Queue is empty :(");
+//      }
+//    }
+
+  public static void main(String[] args) {
+    Queue newQueue = new Queue();
+
+    newQueue.enqueue(1);
+    newQueue.enqueue(5);
+    newQueue.enqueue(7);
+    newQueue.enqueue(9);
+    newQueue.peek();
+    System.out.println();
+    newQueue.dequeue();
+    newQueue.peek();
+  }
 }
